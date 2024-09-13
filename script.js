f858cbb74017b11c936615b4f40bbcdb61337c4e
Analizfelix3a1 <00001097248677sp@al.educacao.sp.gov.br>
2024-09-13 12:22:52 -0300

    ll

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach9(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+        
+ })
+ })
